39989dec2552b9f438dfc27785a9f6cfc5b99a57
PedroSilvv <pedrosilvestre800@gmail.com>
2026-04-17 00:43:48 -0300

    setup project

---
+++ src/main/java/com/estorilflow/EstorilFlowApplication.java
@@ -0,0 +1,13 @@
+package com.estorilflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstorilFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstorilFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estorilflow/EstorilFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estorilflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstorilFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
